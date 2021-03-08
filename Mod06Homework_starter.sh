7057a6ce305b424dc8d8770a97fe969b3c764290
lukew2251 <59976033+lukew2251@users.noreply.github.com>
2021-03-08 11:04:51 -0600

    Add files via upload

---
+++ Mod06Homework_starter.sh
@@ -0,0 +1,47 @@
+#! /bin/sh
+
+#declare and fill the four suits for the deck
+declare -a spades=('Ace_of_Spades' 'King_of_Spades' \
+            'Queen_of_Spades' 'Jack_of_Spades' \
+            '10_of_Spades' '9_of_Spades' \
+            '8_of_Spades' '7_of_Spades' \
+            '6_of_Spades' '5_of_Spades' \
+            '4_of_Spades' '3_of_Spades' \
+            '2_of_Spades')
+
+declare -a diamonds=('Ace_of_Diamonds' 'King_of_Diamonds' \
+            'Queen_of_Diamonds' 'Jack_of_Diamonds' \
+            '10_of_Diamonds' '9_of_Diamonds' \
+            '8_of_Diamonds' '7_of_Diamonds' \
+            '6_of_Diamonds' '5_of_Diamonds' \
+            '4_of_Diamonds' '3_of_Diamonds' \
+            '2_of_Diamonds')
+
+declare -a clubs=('Ace_of_Clubs' 'King_of_Clubs' \
+            'Queen_of_Clubs' 'Jack_of_Clubs' \
+            '10_of_Clubs' '9_of_Clubs' \
+            '8_of_Clubs' '7_of_Clubs' \
+            '6_of_Clubs' '5_of_Clubs' \
+            '4_of_Clubs' '3_of_Clubs' \
+            '2_of_Clubs')
+			
+declare -a hearts=('Ace_of_Hearts' 'King_of_Hearts' \
+            'Queen_of_Hearts' 'Jack_of_Hearts' \
+            '10_of_Hearts' '9_of_Hearts' \
+            '8_of_Hearts' '7_of_Hearts' \
+            '6_of_Hearts' '5_of_Hearts' \
+            '4_of_Hearts' '3_of_Hearts' \
+            '2_of_Hearts')
+
+
+
+
+
+
+
+
+
+
+
+
+
