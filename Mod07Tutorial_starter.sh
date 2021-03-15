2a0f35ab7f12dfb66d6249afa62a6e67f57c41f1
lukew2251 <59976033+lukew2251@users.noreply.github.com>
2021-03-15 11:17:06 -0500

    Add files via upload

---
+++ Mod07Tutorial_starter.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+lyrics_string="Quisiera:Ayer:cambiarle:conocí:el:un:final
+:cielo:al:sin:cuento|Las:sol|Y:barras:un:y:hombre:los:sin
+:tragos:suelo|Un:han:santo:sido:en:testigo|De:prision|Y:el
+:una:dolor:canción:que:triste:me:sin:causaste:dueño|Y:y
+:conocí:to':tus:lo:ojos:que:negros|Y:hciste:ahora:conmigo|Un
+:sí:infeliz:que:en:no:el:puedo:amor,:vivir:que:sin:aun
+:ellos:no:yo|Le:te:pido:supera|Que:al:ahora:cielo:camina
+:solo:solo:un:sin:deseo|Que:nadie:en:por:tus:todas:ojos
+:las:yo:aceras|Preguntándole:pueda:a:vivir|He:Dios:recorrido
+:si:el:en:mundo:verad:entero|te:el:vengo:amor:a:existe|:decir|"
